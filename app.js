cec11336fbdec557022f004ae93f8e98ba7572c2
hrome <58020216+hromero7@users.noreply.github.com>
2020-05-07 21:58:22 -0700

    added functionality for adding an employee, department, and role

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cTable = require ("console.table");
 connection;
 
 const queryAllEmployees = function () {
-    connection.query("SELECT first_name, last_name, title, name, salary, manager_id FROM employee LEFT JOIN role ON (employee.role_id = role.id) JOIN department ON (department.id = role.department_id)", function(err, res) {
+    connection.query("SELECT employee.id, first_name, last_name, title, name, salary, manager_id FROM employee LEFT JOIN role ON (employee.role_id = role.id) JOIN department ON (department.id = role.department_id)", function(err, res) {
         if (err) throw err;
         console.table(res);
         connection.end();
@@ -14,22 +14,66 @@ const queryAllEmployees = function () {
 }
 
 const findByDepartment = function (userChoice) {
-    connection.query("SELECT first_name, last_name, title, name, salary FROM department LEFT JOIN role ON (department.id = role.department_id) JOIN employee ON (employee.role_id = role.id) WHERE (department.name = ?)", [userChoice], function(err, res) {
+    connection.query("SELECT employee.id, first_name, last_name, title, name, salary FROM department LEFT JOIN role ON (department.id = role.department_id) JOIN employee ON (employee.role_id = role.id) WHERE (department.name = ?)", [userChoice], function(err, res) {
         console.table(res);
     })
     connection.end();
 }
 
 const findAllRoles = function (roleChoice) {
-    connection.query("SELECT first_name, last_name, title, name, salary, manager_id FROM employee LEFT JOIN role ON (employee.role_id = role.id) JOIN department ON (department.id = role.department_id) WHERE (role.title = ?)", [roleChoice], function(err, res){
+    connection.query("SELECT employee.id, first_name, last_name, title, name, salary, manager_id FROM employee LEFT JOIN role ON (employee.role_id = role.id) JOIN department ON (department.id = role.department_id) WHERE (role.title = ?)", [roleChoice], function(err, res){
         console.table(res);
         connection.end();
     })
 }
 
+const addNewEmployee = function(answers, roleId) {
+    if(answers.manager === "None") answers.manager =  null;
+    connection.query(
+        "INSERT INTO employee SET ?",
+        {
+            first_name: answers.firstName,
+            last_name: answers.lastName,
+            role_id: roleId,
+            manager_id: answers.manager
+        },
+        function(err) {
+            if (err) throw err;
+            console.log(`${answers.firstName} ${answers.lastName} was successfully added to the system.`)
+        })
+}
 
+const addNewDepartment = function(departmentName) {
+   connection.query( 
+       "INSERT INTO department SET ?",
+    {
+        name: departmentName
+    },
+    function(err) {
+        if (err) throw err;
+        console.log(`Added ${departmentName} to the database.`)
+    })
+}
+
+const addNewRole = function(answers, departmentId) {
+    connection.query(
+        "INSERT INTO role SET ?",
+        {
+            title: answers.title,
+            salary: answers.salary,
+            department_id: departmentId
+        },
+        function(err) {
+            if (err) throw err;
+            console.log(`${answers.title} successfully added to the database.`)
+        }
+    )
+}
 module.exports = {
     queryAllEmployees,
     findByDepartment,
-    findAllRoles
+    findAllRoles,
+    addNewEmployee,
+    addNewDepartment,
+    addNewRole
 };
--- index.js
+++ index.js
@@ -35,14 +35,15 @@ function mainMenu() {
             break;
         
         case "Add an Employee":
-            //query function
+            addEmployee();
             break;
         
         case "Add a Department":
-            //query
+            addDepartment();
             break;
         case "Add a Role":
             //query
+            addRole();
             break;
 
         case "Update Employee Role":
@@ -96,6 +97,98 @@ function roleSearch() {
 }
 
 function addEmployee() {
+    connection.query("SELECT * FROM employee LEFT JOIN role ON (employee.role_id = role.id) ORDER BY employee.manager_id", function(err, res) {
+        inquirer.prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "What is the employee's first name?"
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "What is the employee's last name?"
+            },
+            {
+                name: "role",
+                type: "list",
+                choices: function() {
+                    const choiceArr = [];
+                    for (let i = 0; i < res.length; i++){
+                        choiceArr.push(res[i].title);
+                        
+                    }
+                    return choiceArr;
+                },
+                message: "What is the employee's role?"
+            },
+            {
+                name: "manager",
+                type: "list",
+                choices: function() {
+                    const managerArr = [];
+                    for (let i = 0; i < res.length; i++){
+                        if(res[i].manager_id === null) res[i].manager_id = "None";
+                        managerArr.push(res[i].manager_id);
+                    }
+                    return managerArr;
+                },
+                message: "Who is the employee's manager?"
+                
+            }
+        ]).then(function(answers){
+            // console.log(answers.role);
+            connection.query("SELECT * FROM employee LEFT JOIN role ON (employee.role_id = role.id) WHERE (role.title) = ?", [answers.role], function(err, res){
+                const roleId = res[0].role_id;
+                queryFunctions.addNewEmployee(answers, roleId);
+            })
+        })
+    })
+}
+
+function addDepartment() {
+    inquirer.prompt({
+        name: "name",
+        type: "input",
+        message: "What is the name of the department?"
+    }).then(function(answer){
+        const departmentName = answer.name;
+        queryFunctions.addNewDepartment(departmentName);
+    })
+}
+
+function addRole() {
+    connection.query("SELECT * FROM department", function(err, res){
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the name of the role?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of the role?"
+            },
+            {
+                name: "department",
+                type: "list",
+                choices: function() {
+                    const choiceArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        choiceArr.push(res[i].name);
+                    }
+                    return choiceArr;
+                },
+                message: "Which department does the role belong to?"
+            }
+        ]).then(function (answers){
+            connection.query("SELECT * FROM department WHERE (department.name) = ?", [answers.department], function(err, res){
+                const departmentId = res[0].id;
+                queryFunctions.addNewRole(answers, departmentId);
+            })
+        })
+    })
     
 }
 mainMenu();
